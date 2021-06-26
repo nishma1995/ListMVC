67179dea896f5bc0e6ecca0c7cf1ac1daca07677
nishma1995 <nishmakk@gmail.com>
2021-06-26 15:23:58 -0700

    Add project files.

---
+++ ListMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ListMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Person> personList = new List<Person>();
+
+            Person person1 = new Person();
+            person1.Id = 1;
+            person1.Name = "nishma";
+
+            Person person2 = new Person();
+            person2.Id = 2;
+            person2.Name = "Manu";
+
+            Person person3 = new Person();
+            person3.Id = 3;
+            person3.Name = "jithu";
+            personList.Add(person1);
+            personList.Add(person2);
+            personList.Add(person3);
+
+
+            //ViewBag.personList = personList;
+            return View(personList);
+        }
+    }
+}
---
+++ ListMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ListMVC.MvcApplication" Language="C#" %>
